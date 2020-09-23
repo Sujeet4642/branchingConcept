a3624c285730c15319f3338cdea83c92513250a5
Sujeet4642 <sujeet22s1990@gmail.com>
2020-09-23 19:22:38 +0530

    fixed as per pull request

--- src/test/java/ClassSecond.java
+++ src/test/java/ClassSecond.java
@@ -3,5 +3,6 @@ public class ClassSecond {
 //adding another class
 	//add some more comment
 	//changing something in class and it only shows in merging-branch
+	//fixed the code and ready for merge
 }
 
