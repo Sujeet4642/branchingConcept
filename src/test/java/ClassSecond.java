f0e336afcd437a9e351bc45bfc5be16be12a6cf7
Sujeet4642 <sujeet22s1990@gmail.com>
2020-09-23 19:00:22 +0530

    add one more class and make changes in existing class in merging-branch

--- src/test/java/ClassSecond.java
+++ src/test/java/ClassSecond.java
@@ -2,5 +2,6 @@
 public class ClassSecond {
 //adding another class
 	//add some more comment
+	//changing something in class and it only shows in merging-branch
 }
 
---
+++ src/test/java/MergingBranchClass.java
@@ -0,0 +1,5 @@
+
+public class MergingBranchClass {
+
+	//that class only shows in merging-branch not in master branch.
+}
